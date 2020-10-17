08a3d9f2a1f45bfcb85025dbdd7d8cc478bb9971
appleEyeBall <olasebikane@gmail.com>
2020-10-17 01:34:39 -0500

    - Created new class BetCardAnimation.java to handle all animations - pause/play now works - did some cleaning up and refactoring - removed setnewlooks functionality for pull-request debugging purposes. I want to merege only when you have fixed the issues

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,11 @@
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 // TODO: Create new look menu in GameSceneController, and pass the menuBar into GameSceneController
@@ -16,11 +14,8 @@ public class Controller implements EventHandler{
     Pane welcomeScene;
     VBox gameScene;
     Stage primaryStage;
+    Menu newLookMenu;
     Button startGameBtn;
-    Menu gameSceneMenu;
-    MenuBar gameSceneMenuBar;
-    GameSceneController gameSceneController;
-    int countLookChanges =0; // for back and forth Look changes
 
     public Controller(Stage primaryStage){
         this.primaryStage = primaryStage;
@@ -29,18 +24,17 @@ public class Controller implements EventHandler{
 
         //TODO: delete these 2 lines of code. They make it so
         // I don't have to click to go tho game screen
-        //primaryStage.setScene(new Scene(gameScene,Util.width,Util.height));
-        gameSceneController = new GameSceneController(gameScene);
-        setNewLooks(false);
+        primaryStage.setScene(new Scene(gameScene,Util.width,Util.height));
+        GameSceneController gameSceneController = new GameSceneController(gameScene);
     }
 
 
     private void setSceneUp(){
         welcomeScene = new AnchorPane();
         gameScene = new VBox();
-        MenuItem newLookMenuItem = new MenuItem("New Look");
-        newLookMenuItem.setId("newLookMenu");
-        newLookMenuItem.setOnAction(this);
+        newLookMenu = new Menu("New Look");
+        newLookMenu.setId("newLookMenu");
+
 
         startGameBtn = new Button();
         startGameBtn.setText("Start Game");
@@ -51,18 +45,17 @@ public class Controller implements EventHandler{
 
         welcomeScene.getChildren().add(this.createMenuBar());
 
-        // create menuBar for game scene (welcome Menu + newLooks option)
-        gameSceneMenu = this.createMenuBar().getMenus().get(0);
-        gameSceneMenu.getItems().add(newLookMenuItem);
-        gameSceneMenuBar = new MenuBar(gameSceneMenu);
-        gameScene.getChildren().add(gameSceneMenuBar);
+        // create menuBar for main game scene, and add to scene
+        MenuBar gameMenuBar = this.createMenuBar();
+        gameMenuBar.getMenus().add(newLookMenu);
+        gameScene.getChildren().add(gameMenuBar);
 
         primaryStage.setTitle("Keno Game");
         primaryStage.setScene(new Scene(welcomeScene, Util.width, Util.height));
         primaryStage.show();
     }
 
-
+    //
     public void handle(Event event) {
 
         if (event.getSource() == startGameBtn){
@@ -74,15 +67,6 @@ public class Controller implements EventHandler{
         }
         else if (((MenuItem)event.getSource()).getId() == "oddsBtn"){
             displayGameInfo("Odds of Winning the Game:",Util.oddsOfWinning);
-        }
-        else if (((MenuItem)event.getSource()).getId() == "newLookMenu"){    //event handling for new Looks menu item in the game scene menu
-            countLookChanges++;
-            if(countLookChanges%2 ==0){ //switch back to old look
-                setNewLooks(false);
-            }
-            else{            // set a new look
-                setNewLooks(true);
-            }
         }
 
         else {
@@ -126,24 +110,6 @@ public class Controller implements EventHandler{
         alert.setContentText(menuInfo);
         alert.show();
         alert.getDialogPane().getButtonTypes().add(ButtonType.OK);
-
-    }
-
-    // change the looks of the game scene
-    public void setNewLooks(Boolean newLooksStatus){
-
-        if(newLooksStatus) { // if the button is pressed to set a new look
-            gameSceneController.spotsBox.setBackground(new Background(new BackgroundFill(Color.LIGHTCYAN, CornerRadii.EMPTY, Insets.EMPTY)));
-            gameSceneController.drawingsRow.setBackground(new Background(new BackgroundFill(Color.LIGHTCYAN, CornerRadii.EMPTY, Insets.EMPTY)));
-            gameSceneController.gameScene.setBackground(new Background(new BackgroundFill(Color.LEMONCHIFFON, CornerRadii.EMPTY, Insets.EMPTY)));
-            gameSceneController.betCard.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-        }
-        else{   // if the button is pressed to switch back to previous look
-            gameSceneController.gameScene.setBackground(new Background(new BackgroundFill (Color.LIGHTSKYBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-            gameSceneController.betCard.setBackground(new Background(new BackgroundFill (Color.LEMONCHIFFON, CornerRadii.EMPTY, Insets.EMPTY)));
-            gameSceneController.spotsBox.setBackground(new Background(new BackgroundFill (Color.LIGHTCYAN, CornerRadii.EMPTY, Insets.EMPTY)));
-            gameSceneController.drawingsRow.setBackground(new Background(new BackgroundFill (Color.LIGHTCYAN, CornerRadii.EMPTY, Insets.EMPTY)));
-        }
 
     }
 
